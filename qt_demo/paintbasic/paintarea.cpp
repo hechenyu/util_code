c488c029fd12f0517eb2200eccda618660674543
hexu <hexu_bupt@sina.com>
2016-12-25 16:09:15 +0800

    完成drawArrowLine

--- qt_demo/paintbasic/paintarea.cpp
+++ qt_demo/paintbasic/paintarea.cpp
@@ -48,46 +48,41 @@ void drawArrowLine(QPainter &painter, const QPoint &p1, const QPoint &p2) // fro
     int width = 10;     // 箭头宽度
     int length = 12;    // 箭头长度
     QPoint pa, pb, pc;  // pa为箭头顶点, pb, pc为两边的顶点
-    if (p1.x() == p2.x()) { // k1为无穷, k2为0
-        if (p1.y() < p2.y()) {
-        } else {
-        }
-    } else if (p1.y() == p2.y()){ // k1为0, k2为无穷
-    } else { 
-        double k1 = ((double) (p2.y() - p1.y())) / ((double) (p2.x() - p1.x())); // p1->p2的斜率
-        double k2 = -1 / k1;    // p1->p2垂直线的斜率
+    double sin1, cos1, sin2, cos2;
+    if (p1.x() == p2.x()) {         // 垂直直线
+        cos1 = 0;
+        sin1 = p1.y() < p2.y() ? 1 : -1;
+        cos2 = 1;
+        sin2 = 0;
+    } else if (p1.y() == p2.y()) {  // 水平直线
+        cos1 = p1.x() < p2.x() ? 1 : -1;
+        sin1 = 0;
+        cos2 = 0;
+        sin2 = 1;
+    } else {                        // 斜直线
+        // 直线斜率
+        double k1 = ((double) (p2.y() - p1.y())) / ((double) (p2.x() - p1.x()));
+        double k2 = -1 / k1;    // 法线斜率
         clog << "k1: " << k1 << ", k2: " << k2 << '\n';
-        double sin1, cos1, sin2, cos2;
         // k = tan, cos = +-sqrt(1/(1+k*k)), sin = +-sqrt(k*k/(1+k*k));
-        cos1 = sqrt(1 / (1 + k1*k1));
-        sin1 = sqrt((k1 * k1) / (1 + k1*k1));
-        if (k2 > 0) {   // k2 > 0
-            cos2 = sqrt(1 / (1 + k2*k2));
-            sin2 = sqrt((k2 * k2) / (1 + k2*k2));
-        } else {        // k2 < 0
-            cos2 = sqrt(1 / (1 + k2*k2));
-            sin2 = -sqrt((k2 * k2) / (1 + k2*k2));
-        }
-        clog << "cos1: " << cos1 << ", sin1: " << sin1 << '\n';
-        clog << "cos2: " << cos2 << ", sin2: " << sin2 << '\n';
-        if (p1.x() < p2.x()) {
-            pa.setX(cos1*length+pm.x());
-        } else {
-            pa.setX(-cos1*length+pm.x());
-        }
-        if (p1.y() < p2.y()) {
-            pa.setY(sin1*length+pm.y());
-        } else {
-            pa.setY(-sin1*length+pm.y());
-        }
-        pb.setX(cos2*width/2+pm.x());
-        pb.setY(sin2*width/2+pm.y());   // (pb.y - pm.y) / (width/2) = sin2
-        pc.setX(-cos2*width/2+pm.x());
-        pc.setY(-sin2*width/2+pm.y());  // (pm.y - pc.y) / (width/2) = sin2
+        cos1 = (p1.x() < p2.x() ? 1 : -1) * sqrt(1 / (1 + k1*k1));
+        sin1 = (p1.y() < p2.y() ? 1 : -1) * sqrt((k1 * k1) / (1 + k1*k1));
+        cos2 = sqrt(1 / (1 + k2*k2));
+        sin2 = (k2 > 0 ? 1 : -1) * sqrt((k2 * k2) / (1 + k2*k2));
     }
+    clog << "cos1: " << cos1 << ", sin1: " << sin1 << '\n';
+    clog << "cos2: " << cos2 << ", sin2: " << sin2 << '\n';
+
+    pa.setX(cos1*length+pm.x());
+    pa.setY(sin1*length+pm.y());
+    pb.setX(cos2*width/2+pm.x());
+    pb.setY(sin2*width/2+pm.y());   // (pb.y - pm.y) / (width/2) = sin2
+    pc.setX(-cos2*width/2+pm.x());
+    pc.setY(-sin2*width/2+pm.y());  // (pm.y - pc.y) / (width/2) = sin2
     clog << "pa: (" << pa.x() << ", " << pa.y() << ")\n";
     clog << "pb: (" << pb.x() << ", " << pb.y() << ")\n";
     clog << "pc: (" << pc.x() << ", " << pc.y() << ")\n";
+
     painter.drawLine(pa, pb);
     painter.drawLine(pb, pc);
     painter.drawLine(pc, pa);
