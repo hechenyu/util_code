88bfd1c8074f393106625b0c77510e772954e729
hexu <hexu_bupt@sina.com>
2017-06-20 01:59:32 +0800

    implement unordered_map_extern_c

--- algo_demo/unordered_map_extern_c/lib/unordered_map_extern_c.cpp
+++ algo_demo/unordered_map_extern_c/lib/unordered_map_extern_c.cpp
@@ -7,7 +7,7 @@
 namespace {
 
 inline 
-int default_equal_func(unordered_map_key_t *key1, unordered_map_key_t *key2)
+int default_equal_func(const unordered_map_key_t *key1, const unordered_map_key_t *key2)
 {
     size_t len = std::min(key1->len, key2->len);
     return memcmp(key1->key, key2->key, len) == 0 && key1->len == key2->len;
@@ -19,16 +19,16 @@ struct key_equal {
     key_equal(): comp(default_equal_func) {}
     key_equal(equal_func *f): comp(f) {}
 
-    bool operator ()(unordered_map_key_t key1, unordered_map_key_t key2)
+    bool operator ()(const unordered_map_key_t &key1, const unordered_map_key_t &key2) const
     {
         return comp(&key1, &key2) == 0;
     }
 };
 
 inline
-size_t default_hash_func(unordered_map_key_t *key)
+size_t default_hash_func(const unordered_map_key_t *key)
 {
-    return hash_function::hash(key->key, key->len);
+    return hash_function::hash((void *) key->key, key->len);
 }
 
 struct hasher {
@@ -37,7 +37,7 @@ struct hasher {
     hasher(): hash(default_hash_func) {}
     hasher(hash_func *f): hash(f) {}
 
-    size_t operator ()(unordered_map_key_t key)
+    size_t operator ()(const unordered_map_key_t &key) const
     {
         return hash(&key);
     }
@@ -45,8 +45,47 @@ struct hasher {
 
 }   // namespace {
 
-#if 0
 struct unordered_map_s {
-    std::unordered_map<unordered_map_key_t, unordered_map_value_t, hasher, key_equal>
+    std::unordered_map<unordered_map_key_t, unordered_map_value_t, hasher, key_equal> map;
+
+    explicit unordered_map_s(size_t n): map(n) {}
 };
-#endif
+
+unordered_map_t *unordered_map_create(size_t n)
+{
+    return new unordered_map_s(n);
+}
+
+void unordered_map_destroy(unordered_map_t *map)
+{
+    delete map;
+}
+
+int unordered_map_insert(unordered_map_t *map, unordered_map_key_t *key, unordered_map_value_t *val)
+{
+    auto ret = map->map.emplace(*key, val);
+    return !ret.second;
+}
+
+unordered_map_value_t unordered_map_find(unordered_map_t *map, unordered_map_key_t *key)
+{
+    auto iter = map->map.find(*key);
+    if (iter == map->map.end()) {
+        return NULL;
+    }
+
+    return iter->second;
+}
+
+unordered_map_value_t unordered_map_delete(unordered_map_t *map, unordered_map_key_t *key)
+{
+    auto iter = map->map.find(*key);
+    if (iter == map->map.end()) {
+        return NULL;
+    }
+
+    auto ret = iter->second;
+    map->map.erase(iter);
+    return ret;
+}
+
--- algo_demo/unordered_map_extern_c/lib/unordered_map_extern_c.h
+++ algo_demo/unordered_map_extern_c/lib/unordered_map_extern_c.h
@@ -7,20 +7,20 @@ struct unordered_map_s;
 typedef struct unordered_map_s unordered_map_t;
 
 typedef struct unordered_map_key_s {
-    const void *key;        // 指向key所在内存的指针
+    const void *key;        // 指向key所在内存的指针, key指向的内存由调用者维护
     size_t len;             // key的长度
 } unordered_map_key_t;
 
-typedef void *unordered_map_value_t;
+typedef void *unordered_map_value_t;    // value的内存由调用者维护
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 // 如果两个key相等, 返回0, 其他返回非0
-typedef int equal_func(unordered_map_key_t *, unordered_map_key_t *);
+typedef int equal_func(const unordered_map_key_t *, const unordered_map_key_t *);
 
-typedef size_t hash_func(unordered_map_key_t *);
+typedef size_t hash_func(const unordered_map_key_t *);
 
 // 创建一个unordered_map
 unordered_map_t *unordered_map_create(size_t n);
@@ -31,15 +31,15 @@ void unordered_map_destroy(unordered_map_t *map);
 // 向hash表里插入一项
 // 如果成功, 返回0,
 // 如果已经存在相同key的项, 返回非0
-int unordered_map_insert(unordered_map_t *map, unordered_map_key_t *key, unordered_map_value_t *value);
+int unordered_map_insert(unordered_map_t *map, unordered_map_key_t *key, unordered_map_value_t val);
 
 // 从hash表里查找一项
 // 如果没有符合的项, 返回NULL
-unordered_map_value_t *unordered_map_find(unordered_map_t *map, unordered_map_key_t *key);
+unordered_map_value_t unordered_map_find(unordered_map_t *map, unordered_map_key_t *key);
 
 // 从hash表里删除一项
 // 如果没有符合的项, 返回NULL
-unordered_map_value_t *unordered_map_delete(unordered_map_t *map, unordered_map_key_t *key);
+unordered_map_value_t unordered_map_delete(unordered_map_t *map, unordered_map_key_t *key);
 
 #ifdef __cplusplus
 }   // extern "C" {
